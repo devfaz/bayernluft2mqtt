93a362c3b5c0b731b439a2e1fd5478c93d12d6bc
Fabian Zimmermann <dev.faz@gmail.com>
2025-09-29 12:29:49 +0200

    fix: move latest tag

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,5 +3,7 @@ set -e -o pipefail -u
 
 TAG="v$1"
 
-docker build --pull -t $IMAGE:$TAG .
+docker build -t $IMAGE:$TAG .
 docker push $IMAGE:$TAG
+docker tag $IMAGE:$TAG $IMAGE:latest
+docker push $IMAGE:latest
